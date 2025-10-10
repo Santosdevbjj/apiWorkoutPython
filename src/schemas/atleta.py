9633cdc98a3d0cb24166f9b3b80f6c6f05c80a2b
Sergio <santossergiorealbjj@outlook.com>
2025-10-10 14:04:44 -0300

    Delete src directory

--- src/schemas/atleta.py
+++
@@ -1,9 +0,0 @@
-# Exemplo em src/schemas/atleta.py
-
-class AtletaGetCustomizado(BaseSchema):
-    nome: str = Field(description='Nome do atleta')
-    centro_treinamento: str = Field(description='Nome do Centro de Treinamento')
-    categoria: str = Field(description='Nome da Categoria')
-
-    # Configuração Pydantic (ou usando o .model_validate() no endpoint) para 
-    # mapear os dados do Model ORM para este Schema
